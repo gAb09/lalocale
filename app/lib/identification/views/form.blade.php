9a2c5c996bee78d575ca835219cf3812638206bf
gAb09 <brunogabiot@club-internet.fr>
2015-02-13 22:03:17 +0100

    reprise du chargement des assetsé

--- app/lib/identification/views/form.blade.php
+++ app/lib/identification/views/form.blade.php
@@ -7,7 +7,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
 	<link href="/assets/css/bootstrap-responsive.min.css" rel="stylesheet">
-	<link href="/assets/tresorerie/css/tresorerie.css" rel="stylesheet" type="text/css">
+	<link href="/assets/tresorerie/css/base.css" rel="stylesheet" type="text/css">
+	<link href="/assets/tresorerie/css/layout.css" rel="stylesheet" type="text/css">
 	<link href="/assets/css/style.css" rel="stylesheet" type="text/css">
 
 </head>
